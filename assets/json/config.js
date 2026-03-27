40918798d314fb07c5612eafbb561af7c07ae5d3
bxx-114514 <tzhong072@gmail.com>
2026-03-27 21:40:49 +0900

    Init

---
+++ assets/json/config.js
@@ -0,0 +1,7 @@
+window.WAKATIME_CONFIG = {
+  "date": "2026-02-08",
+  "hours": 0,
+  "theme_name": "rest",
+  "theme_display": "休息日",
+  "updated_at": "2026-02-09T03:16:13.580Z"
+};
---
+++ assets/json/weekly.js
@@ -0,0 +1,56 @@
+window.WAKATIME_WEEKLY = {
+  "updated_at": "2026-02-09T03:16:14.951Z",
+  "stats": {
+    "total_hours": 31.15,
+    "daily_avg": 4.45,
+    "trend": "falling",
+    "max_day": {
+      "date": "2026-02-04",
+      "hours": 7.72,
+      "text": "7 hrs 43 mins"
+    }
+  },
+  "days": [
+    {
+      "date": "2026-02-03",
+      "hours": 6.34,
+      "text": "6 hrs 20 mins"
+    },
+    {
+      "date": "2026-02-04",
+      "hours": 7.72,
+      "text": "7 hrs 43 mins"
+    },
+    {
+      "date": "2026-02-05",
+      "hours": 7.62,
+      "text": "7 hrs 36 mins"
+    },
+    {
+      "date": "2026-02-06",
+      "hours": 7.57,
+      "text": "7 hrs 33 mins"
+    },
+    {
+      "date": "2026-02-07",
+      "hours": 0,
+      "text": "0 secs"
+    },
+    {
+      "date": "2026-02-08",
+      "hours": 0,
+      "text": "0 secs"
+    },
+    {
+      "date": "2026-02-09",
+      "hours": 1.9,
+      "text": "1 hr 54 mins"
+    }
+  ],
+  "ai": {
+    "title": "能量流失",
+    "quote": "代码就像夜色，越写越暗，越写越凉。",
+    "tarot": "🕳️ The Hermit",
+    "theme_color": "#00FFF7"
+  }
+};
